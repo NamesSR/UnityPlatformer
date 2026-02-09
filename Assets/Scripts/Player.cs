75e2a8b452388f84e357e987694c39cf8948cd5b
Names <40932@ma-web.nl>
2026-02-09 15:39:06 +0100

    episode 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,9 +11,12 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private bool isGrounded;
 
+    private Animator animator;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
   
@@ -21,13 +24,41 @@ public class Player : MonoBehaviour
     {
         float MoveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(MoveInput * Movepeed, rb.linearVelocity.y);
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
         }
+
+        SetAnimation(MoveInput);
     }
     private void FixedUpdate()
     {
       isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+    private void SetAnimation(float MoveInput)
+    {
+        if (isGrounded)
+        {
+            if (MoveInput == 0)
+            {
+                animator.Play("Player_Idle");
+            }
+            else
+            {
+                animator.Play("Player_run");
+            }
+        }
+        else
+        {
+            if(rb.linearVelocityY > 0)
+            {
+                animator.Play("Player_Jump");
+            }
+            else
+            {
+                animator.Play("Player_fall");
+            }
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # UnityPlatformer
 
+# Episode 1
 ## ![gif](image/episode1.gif)
+### [Player code](Assets/Scripts/Player.cs)
+
+
+#
+#
+# Episode 2
+## ![gif](image/episode2.gif)
 ### [Player code](Assets/Scripts/Player.cs)
